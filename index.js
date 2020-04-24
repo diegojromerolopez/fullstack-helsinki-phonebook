b1d75c27dedf9d6b9d77e1154f424de2ae546957
Diego J <diegojromerolopez@gmail.com>
2020-04-24 19:59:04 +0200

    Ready for heroku

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
-morgan.token('body', (req, res) => JSON.stringify(req.body) );
-
 app.use(express.json())
+app.use(cors())
 
+morgan.token('body', (req, res) => JSON.stringify(req.body) );
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
 
 let persons = [
@@ -72,7 +73,7 @@ let persons = [
     response.status(204).end()
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
