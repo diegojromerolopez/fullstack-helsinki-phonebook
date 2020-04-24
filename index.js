da6100221a0539244700ea44fd73fe3fde4ba6f8
Diego J <diegojromerolopez@gmail.com>
2020-04-24 19:33:50 +0200

    part3.8

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const morgan = require('morgan')
 
 const app = express()
 
+morgan.token('body', (req, res) => JSON.stringify(req.body) );
+
 app.use(express.json())
-app.use(morgan('tiny'))
+
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
 
 let persons = [
     {
